77ef3ba5e625551e0f849dc738fdcbf691725000
FaaOfc <159248870+FaaOfc@users.noreply.github.com>
2025-06-09 21:39:38 +0700

    Update script.js

--- special-char/script.js
+++ special-char/script.js
@@ -1,14 +1,13 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+      e.preventDefault();
+      const username = document.getElementById("username").value.trim().toLowerCase();
+      const password = document.getElementById("password").value;
 
-  const username = document.getElementById("username").value.trim().toLowerCase();
-  const password = document.getElementById("password").value;
-
-  if (username === "zann" && password === "ibukiforme") {
-    window.location.href = "./ibuki"; // redirect ke folder ibuki
-  } else if (username === "faa" && password === "hutaoismywife") {
-    window.location.href = "./hutao"; // redirect ke folder hutao
-  } else {
-    alert("Username atau password salah!");
-  }
-});
+      if (username === "zann" && password === "ibukiforme") {
+        window.location.href = "./ibuki";
+      } else if (username === "faa" && password === "hutaoismywife") {
+        window.location.href = "./hutao";
+      } else {
+        alert("Username atau password salah!");
+      }
+    });
